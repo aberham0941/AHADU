41da3e5d43291ce8ff29a047f35cb07729304673
Eyrusalem <57269969+Eyrusalem@users.noreply.github.com>
2020-01-23 00:47:40 +0300

    Add files via upload

---
+++ body.php
@@ -0,0 +1,414 @@
+
+   </php
+session_start();
+
+/>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		 <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+		<title>AHADU PAY</title>
+
+		<!-- Google font -->
+		<link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700" rel="stylesheet">
+
+		<!-- Bootstrap -->
+		<link type="text/css" rel="stylesheet" href="css/bootstrap.min.css"/>
+
+		<!-- Slick -->
+		<link type="text/css" rel="stylesheet" href="css/slick.css"/>
+		<link type="text/css" rel="stylesheet" href="css/slick-theme.css"/>
+
+		<!-- nouislider -->
+		<link type="text/css" rel="stylesheet" href="css/nouislider.min.css"/>
+
+		<!-- Font Awesome Icon -->
+		<link rel="stylesheet" href="css/font-awesome.min.css">
+
+		<!-- Custom stlylesheet -->
+		<link type="text/css" rel="stylesheet" href="css/style.css"/>
+		<link type="text/css" rel="stylesheet" href="css/accountbtn.css"/>
+		
+		
+		<style>
+        #navigation {
+          background: #FF4E50;  /* fallback for old browsers */
+            background: -webkit-linear-gradient(to right, #F9D423, #FF4E50);  /* Chrome 10-25, Safari 5.1-6 */
+            background: linear-gradient(to right, #F9D423, #FF4E50); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+          
+        }
+        #header {
+  
+            background: #780206;  /* fallback for old browsers */
+            background: -webkit-linear-gradient(to right, #061161, #780206);  /* Chrome 10-25, Safari 5.1-6 */
+            background: linear-gradient(to right, #061161, #780206); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+  
+        }
+        #top-header {
+              
+  
+            background: #870000;  /* fallback for old browsers */
+            background: -webkit-linear-gradient(to right, #190A05, #870000);  /* Chrome 10-25, Safari 5.1-6 */
+            background: linear-gradient(to right, #190A05, #870000); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+
+        }
+        #footer {
+            background: #7474BF;  /* fallback for old browsers */
+            background: -webkit-linear-gradient(to right, #348AC7, #7474BF);  /* Chrome 10-25, Safari 5.1-6 */
+            background: linear-gradient(to right, #348AC7, #7474BF); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+
+          color: #1E1F29;
+        }
+        #bottom-footer {
+            background: #7474BF;  /* fallback for old browsers */
+            background: -webkit-linear-gradient(to right, #348AC7, #7474BF);  /* Chrome 10-25, Safari 5.1-6 */
+            background: linear-gradient(to right, #348AC7, #7474BF); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+          
+
+        }
+        .footer-links li a {
+          color: #1E1F29;
+        }
+        .mainn-raised {
+            
+            margin: -7px 0px 0px;
+            border-radius: 6px;
+            box-shadow: 0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.2);
+
+        }
+       
+        .glyphicon{
+    display: inline-block;
+    font: normal normal normal 14px/1 FontAwesome;
+    font-size: inherit;
+    text-rendering: auto;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    }
+    .glyphicon-chevron-left:before{
+        content:"\f053"
+    }
+    .glyphicon-chevron-right:before{
+        content:"\f054"
+    }
+        
+
+       
+        
+        </style>
+
+    </head>
+	<body>
+		<!-- HEADER -->
+			<!-- MAIN HEADER -->
+			
+      <div id="header">
+				<!-- container -->
+				<div class="container">
+					<!-- row -->
+					<div class="row">
+						<!-- LOGO -->
+						<div class="col-md-3">
+							<div class="header-logo">
+								<a href="#" class="logo">
+								<font style="font-style:normal; font-size: 33px;color: aliceblue;font-family: serif">
+                                        AHADU PAY
+                                    </font>
+									
+								</a>
+							</div>
+						</div>
+						<!-- /LOGO -->
+
+
+   <div class="main main-raised">
+        <div class="container mainn-raised" style="width:100%;">
+  
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+   
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+
+      <div class="item active">
+        <img src="img/ahadu.jpg" style="width:100%;">
+        
+      </div>
+
+      <div class="item">
+        <img src="img/is.jpg" style="width:100%;">
+    </div>
+
+    <!-- Left and right controls -->
+    	<a class="left carousel-control _26sdfg" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only" >Previous</span>
+    </a>
+    <a class="right carousel-control _26sdfg" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+     
+
+
+
+		
+
+		<!-- SECTION -->
+		<div class="section">
+			<!-- container -->
+			<div class="container">
+				<!-- row -->
+				<div class="row">
+
+					<!-- section title -->
+					<div class="col-md-12">
+						<div class="section-title">
+							<h2 class="title">SERVICE DISCRIPTION</h2>
+							<div class="section-nav">
+	
+								
+								<h3 class="text-uppercase">bill payment</h3>
+								
+							<p> We offer a secure and easy way to manage your utility bills. All your personal information is secure at all times. Our developers regularly monitor and update the system for a better experience. We provide highly scalable and secure web billing solutions.</p>
+							
+							</div>
+							
+						</div>
+						
+					</div>
+					<!-- /section title -->
+		
+					</div>
+			<div class="container">
+			<h2>Simple and Convenient<strong> </h2>
+					<p> Easy to use and accessible from anywhere and anytime. The hardship of onsite paying is guaranteed to be avoided through out the whole process..</h>
+						
+			</div>			
+
+						
+					<div>
+							<h3 class="text-uppercase">exchange rate</h3>
+							<p> We collect and organize currency trading data. 
+							We find the best currency rates. We provide tools, peer comparisons and information to reduce currency exchange costs worldwide. 
+							We provide tools, peer comparisons and information to reduce currency exchange costs worldwide. </p>
+							<h3 class="text-uppercase">money transfer</h3>
+							<p> Our easy to use, robust platform streamlines your business processes and daily operations and makes it easy for you to expand your business vision and profits. 
+							Your customers will be increasingly looking to access and transfer money online in a quicker and more convenient way.</p>
+
+								
+							<h3 class="text-uppercase">cerdit service</h3>
+						
+						
+					
+						
+			<div class="container">
+				<!-- row -->
+				<div class="row">
+					<div class="col-md-4 col-xs-6">
+						<div class="section-title">
+							
+							<div class="section-nav">
+								<div id="slick-nav-3" class="products-slick-nav"></div>
+							</div>
+						</div>
+						
+						
+
+						<div class="products-widget-slick" data-nav="#slick-nav-3">
+							<div id="get_product_home">
+								<!-- product widget -->
+								
+							<!	<!-- product widget -->
+							</div>
+							<div class="products-widget-slick" data-nav="#slick-nav-3">
+							<div id="get_product_home">
+						<!-- HOT DEAL SECTION -->
+		<!--<div id="hot-deal" class="section mainn mainn-raised">
+			<!-- container -->
+			<!--<div class="container">
+				<!-- row -->
+				<div class="row">
+					<div class="col-md-12">
+						
+							<ul class="hot-deal-countdown">
+						
+						<h2 class="text-uppercase">Safe and Protected</h2>
+						
+						<div class="item active">
+        					<img src="img/is.jpg" style="width:200%;">
+        
+      					</div>
+						  <div class="col-md-12">
+						
+						<!--<ul class="hot-deal-countdown">-->
+					
+					
+					
+				<!--<div class="item active">
+				  </div>
+
+
+
+							
+							
+						
+					</div>
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
+								
+							</div>
+						</div>
+					</div>
+
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
+		<!-- /SECTION -->
+</div>
+<footer id="footer">
+			<!-- top footer -->
+			<div class="section">
+				<!-- container -->
+				<div class="container">
+					<!-- row -->
+					<div class="row">
+						<div class="col-md-3 col-xs-6">
+							<div class="footer">
+								<h3 class="footer-title">About Us</h3>
+								
+								
+								<ul class="footer-links">
+									<li><a href="#"><i class="fa fa-map-marker"></i>Adama Science and Technology University</a></li>
+									<li><a href="#"><i class="fa fa-phone"></i>0912345678</a></li>
+									<li><a href="#"><i class="fa fa-phone"></i>0911000001</a></li>
+									<li><a href="#"><i class="fa fa-envelope-o"></i>aberhamberhanu@gmail.com</a></li>
+									<li><a href="#"><i class="fa fa-envelope-o"></i>ruth17.et@gmail.com</a></li>
+								</ul>
+							</div>
+						</div>
+						<div class="col-md-6 text-center" style="margin-top:80px;">
+							
+								<span class="copyright">
+								<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+								Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved
+							
+							</span>
+						</div>
+
+						<div class="col-md-3 col-xs-6">
+							<div class="footer">
+								<h3 class="footer-title"> </strong>Qualities</h3>
+								<ul class="footer-links">
+									<li><a href="#">convenient</a></li>
+									<li><a href="#">accesiblity despite several constraints</a></li>
+									<li><a href="#">simple</a></li>
+									<li><a href="#"></a>safe and protected</li>
+									
+								</ul>
+							</div>
+						</div>
+
+						<div class="clearfix visible-xs"></div>
+
+        
+
+						<div id="newsletter" class="section">
+			<!-- container -->
+			<div class="container">
+				<!-- row -->
+				<div class="row">
+					<div class="col-md-12">
+					
+						<div class="newsletter">
+							<p>Sign Up for free and enjoy</p>
+							<form id="offer_form" onsubmit="return false">
+								<input class="input" type="email" id="email" name="email" placeholder="Enter Your Email">
+								<button class="newsletter-btn" value="Sign Up" name="signup_button" type="submit"><i class="fa fa-envelope"></i> Subscribe</button>
+							</form>
+							<div class="" id="offer_msg">
+                                <!--Alert from signup form-->
+                            </div>
+							
+						</div>
+					</div>
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
+
+						
+					</div>
+					<!-- /row -->
+				</div>
+				<!-- /container -->
+			</div>
+			<!-- /top footer -->
+                
+
+			<!-- bottom footer -->
+			
+			<!-- /bottom footer -->
+		</footer>
+		<script src="js/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/slick.min.js"></script>
+		<script src="js/nouislider.min.js"></script>
+		<script src="js/jquery.zoom.min.js"></script>
+		<script src="js/main.js"></script>
+		<script src="js/actions.js"></script>
+		<script src="js/sweetalert.min"></script>
+		<script src="js/jquery.payform.min.js" charset="utf-8"></script>
+    <script src="js/script.js"></script>
+		<script>var c = 0;
+        function menu(){
+          if(c % 2 == 0) {
+            document.querySelector('.cont_drobpdown_menu').className = "cont_drobpdown_menu active";    
+            document.querySelector('.cont_icon_trg').className = "cont_icon_trg active";    
+            c++; 
+              }else{
+            document.querySelector('.cont_drobpdown_menu').className = "cont_drobpdown_menu disable";        
+            document.querySelector('.cont_icon_trg').className = "cont_icon_trg disable";        
+            c++;
+              }
+        }
+           
+		
+</script>
+
+    <script type="text/javascript">
+		$('.block2-btn-addcart').each(function(){
+			var nameProduct = $(this).parent().parent().parent().find('.block2-name').html();
+			$(this).on('click', function(){
+				swal(nameProduct, "is added to cart !", "success");
+			});
+		});
+
+		$('.block2-btn-addwishlist').each(function(){
+			var nameProduct = $(this).parent().parent().parent().find('.block2-name').html();
+			$(this).on('click', function(){
+				swal(nameProduct, "is added to wishlist !", "success");
+			});
+		});
+	</script>
+	
+
+		
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+include "body.php";
+
+
+?>
+		
+		
